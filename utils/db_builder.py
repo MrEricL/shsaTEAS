995d1d09f7539f8d8a5d5df07c8627cb1e21f17c
MrEricL <eli6@stuy.edu>
2018-06-16 15:56:32 -0400

    forum changes

--- utils/db_builder.py
+++ utils/db_builder.py
@@ -3,12 +3,14 @@ import csv
 import hashlib
 import uuid
 import os
+import datetime
 
 
 DIR = os.path.dirname(__file__) or '.'
 DIR += '/../data/data.db'
 
 def tableCreation():
+
     if not os.path.isfile(DIR):
         db = sqlite3.connect(DIR) #open if f exists, otherwise create
         c = db.cursor()         #facilitates db ops
@@ -19,9 +21,20 @@ def tableCreation():
         progress_table = 'CREATE TABLE progress (userID INTEGER, progressInt INTEGER);'
         c.execute(progress_table)
 
+        categories_table = 'CREATE TABLE categories (catID INTEGER, catName TEXT, catDesc TEXT);'
+        c.execute(categories_table)
+
+        topics_table = 'CREATE TABLE topics (userID INTEGER, catID INTEGER, topicID INTEGER, topic_date DATETIME, title TEXT);'
+        c.execute(topics_table)
+
+        posts_table = 'CREATE TABLE posts (userID INTEGER, catID INTEGER, topicID INTEGER, postID INTEGER, postDate DATETIME, body TEXT);'
+        c.execute(posts_table)
+
 
         db.commit()
         db.close()
+
+        addCategories()
 
 #===========================================================================================================================================
 
@@ -200,3 +213,155 @@ def setProgress(ID, progress):
     c.execute(command.format(ID, progress))
     db.commit()
     db.close()
+
+#Forum Stuff========================================================================================================================
+#(catID INTEGER, catName TEXT, catDesc TEXT);
+def addCategories():
+    db = sqlite3.connect(DIR)
+    c = db.cursor()
+    c.execute('INSERT INTO categories VALUES (?,?,?)',[0, 'General Help', 'Post anything that you have help with.'])
+    c.execute('INSERT INTO categories VALUES (?,?,?)',[1, 'English Language Arts Help', 'Post anything to do with the ELA section.'])
+    c.execute('INSERT INTO categories VALUES (?,?,?)',[2, 'Mathematics Section Help', 'Post anything to do with the math section.'])
+    c.execute('INSERT INTO categories VALUES (?,?,?)',[3, 'School Talk', 'Post here to talk about specialized high schools.'])
+    c.execute('INSERT INTO categories VALUES (?,?,?)',[4, 'Miscellaneous', 'Post here about anything else.'])
+
+    db.commit()
+    db.close() 
+
+
+'''
+topics_table = 'CREATE TABLE topics (userID INTEGER, catID INTEGER, topicID INTEGER, topic_date DATETIME, title TEXT);'
+c.execute(topics_table)
+
+posts_table = 'CREATE TABLE posts (userID INTEGER, catID INTEGER, topicID INTEGER, postID INTEGER, title TEXT, body TEXT, postDate DATETIME);'
+c.execute(posts_table)
+
+'''
+
+def addTopic(ID, catID, title, body):
+
+    db = sqlite3.connect(DIR)
+    c = db.cursor()
+
+
+    date = (datetime.datetime.now()).strftime('%Y-%m-%d')
+    topicID = c.execute('SELECT max(topicID) FROM topics WHERE userID = {}'.format(ID)).fetchone()[0]
+
+    if topicID == None:
+        topicID = 0
+    else:
+        topicID = int(topicID) + 1
+
+
+    c.execute('INSERT INTO topics VALUES (?,?,?,?,?)',[ID, catID, topicID, date, title])
+
+
+    db.commit()
+    db.close() 
+
+    addPost(ID, catID, topicID, body)
+
+'''
+topics_table = 'CREATE TABLE topics (userID INTEGER, catID INTEGER, topicID INTEGER, topic_date DATETIME, title TEXT);'
+c.execute(topics_table)
+
+posts_table = 'CREATE TABLE posts (userID INTEGER, catID INTEGER, topicID INTEGER, postID INTEGER, postDate DATETIME, body TEXT);'
+c.execute(posts_table)
+'''
+
+def addPost(ID, catID, topicID, body):
+
+    db = sqlite3.connect(DIR)
+    c = db.cursor()
+
+    date = (datetime.datetime.now()).strftime('%Y-%m-%d')
+    postID = c.execute('SELECT max(postID) FROM posts WHERE topicID = {}'.format(topicID)).fetchone()[0]
+
+    if postID == None:
+        postID = 0
+    else:
+        postID = int(postID) + 1
+
+
+    c.execute('INSERT INTO posts VALUES (?,?,?,?,?,?)',[ID, catID, topicID, postID, date, body])
+
+    db.commit()
+    db.close() 
+
+
+def addToPost (ID, catID, topicID, postID, body):
+
+    db = sqlite3.connect(DIR)
+    c = db.cursor()
+
+    date = (datetime.datetime.now()).strftime('%Y-%m-%d')
+
+    postID = c.execute('SELECT max(postID) FROM posts WHERE topicID = {} AND postID = {}'.format(topicID, postID)).fetchone()[0]
+
+    postID = int(postID) + 1
+
+    c.execute('INSERT INTO posts VALUES (?,?,?,?,?,?)',[ID, catID, topicID, postID, date, body])
+
+    db.commit()
+    db.close() 
+
+
+
+def getAllTopicInCat(catID):
+    db = sqlite3.connect(DIR)
+    c = db.cursor()
+    allTopic = c.execute('SELECT * FROM topics WHERE catID = {};'.format(catID)).fetchall()
+    db.close()
+
+    ret = []
+    for each in allTopic:
+        d = {}
+        d['title'] = each[4]
+        d['topicID'] = each[2]
+        d['userID'] = each[0]
+        ret.append(d)
+
+    print ret
+
+
+def getAllPostInTopic(catID, topicID):
+    db = sqlite3.connect(DIR)
+    c = db.cursor()
+    allPost = c.execute('SELECT * FROM posts WHERE catID = {} AND topicID = {};'.format(catID, topicID)).fetchall()
+    db.close()
+
+    ret = []
+    for each in allPost:
+        d = {}
+        d['body'] = each[5]
+        d['userID'] = each[0]
+        ret.append(d)
+
+    return ret
+
+
+
+
+if __name__ == '__main__':  
+    tableCreation()
+
+    #getAllPostInTopic(0, 0)
+
+    #getAllTopicInCat(0)
+    #addTopic(0, 0, 'Test Title', 'Body')
+
+    #addToPost(0, 0, 0, 0, 'More stuff')
+
+    '''
+    addPost(ID, catID, topicID, body)
+    addPost(ID, catID, topicID, body)
+
+    addTopic(ID, catID, title, body)
+    addPost(ID, catID, topicID, body)
+    addPost(ID, catID, topicID, body)
+    addPost(ID, catID, topicID, body)
+    '''
+
+    pass
+
+
