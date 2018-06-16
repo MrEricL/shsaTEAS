425448abd12983ab2b6f1a0a560b242097651090
MrEricL <eli6@stuy.edu>
2018-06-16 17:54:16 -0400

    calender changes

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 
 from utils.db_builder import *
 from utils.api import *
+from utils.forum import *
 
 app = Flask(__name__)
 
@@ -29,6 +30,7 @@ def root():
 # ------------------- Login -----------------------------------
 @app.route('/login', methods = ['POST','GET'])
 def login():
+
     user = request.form['user']
     #print user
     passw = request.form['pass']
@@ -54,6 +56,8 @@ def login():
 # ------------------- Register ---------------------------------
 @app.route('/register', methods = ['POST', 'GET'])
 def register():
+
+
     user = request.form['user']
     password = request.form['pass']
     name = request.form['name']
@@ -77,6 +81,10 @@ def register():
 # ------------------- Logout ---------------------------------
 @app.route('/logout', methods = ['POST','GET'])
 def logout():
+    try:
+        ID = getUserID(session['user'])
+    except:
+        return redirect( url_for('root'))
 
     print "Session LogOut================"
     print session
@@ -89,13 +97,19 @@ def logout():
 # ------------------- Calendar ---------------------------------
 @app.route('/calendar')
 def calendar():
-
+    try:
+        ID = getUserID(session['user'])
+    except:
+        return redirect( url_for('root'))
     return render_template("calendar.html")
 
 #============================================================================
 @app.route('/home', methods = ['POST','GET'])
 def home():
-
+    try:
+        ID = getUserID(session['user'])
+    except:
+        return redirect( url_for('root'))
 
     ID = getUserID(session['user'])
     #INFO ================================
@@ -103,8 +117,9 @@ def home():
 
     print  'config',getConfig(ID)
 
-    print 'name',getName(ID)
     name = getName(ID)
+    print 'name', name
+    
 
     print 'usertype',getUserType(ID)
 
@@ -113,23 +128,43 @@ def home():
 
 @app.route('/events', methods = ['POST','GET'])
 def events():
-
+    try:
+        ID = getUserID(session['user'])
+    except:
+        return redirect( url_for('root'))
 
     ID = getUserID(session['user'])
     event = table_builder(get_events()).decode('utf-8')
 
     return render_template('events.html', event = event)
 
+
+# 0 = View All Category
+# 1 = View All Topic in Category
+# 2 = View All Post in Topic
 
 @app.route('/forum', methods = ['POST','GET'])
-def forum():
-
+def forum(location=0):
+    try:
+        ID = getUserID(session['user'])
+    except:
+        return redirect( url_for('root'))
 
     ID = getUserID(session['user'])
-    event = table_builder(get_events()).decode('utf-8')
 
-    return render_template('events.html', event = event)
 
+    cat = categoryTableBuilder(getAllCat())
+
+    if location == 0:
+        return render_template('forum.html', cat = cat)
+    else:
+        return render_template('forum.html', cat = cat)
+
+
+@app.route('/forumconfig', methods = ['POST','GET'])
+def forumconfig():
+    print request.args
+    return 'testing'
 
 
 if __name__=='__main__':
